a29c4c7fb01a3494e08f9a34afafbcd78f513cc5
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2021-11-05 08:19:15 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,26 @@ use mysql::prelude::*;
 use mysql::*;
 
 #[derive(Debug)]
+enum PostMode {
+    Unknown,
+    Text,
+    HTML,
+}
+
+#[derive(Debug)]
 struct Article {
     slug: String,
     topic: String,
     title: String,
     author: String,
     date: String,
+    mode: PostMode,
+    text: String,
 }
 
 impl FromRow for Article {
     fn from_row_opt(row: Row) -> std::result::Result<Article, FromRowError> {
-        let article = Article {
+        Ok(Article {
             slug: row
                 .get_opt(0)
                 .unwrap()
@@ -28,17 +37,15 @@ impl FromRow for Article {
                 .unwrap()
                 .map_err(|_| FromRowError(row.clone()))?,
             author: row
-                .get_opt(3)
+                .get_opt::<String, usize>(3)
                 .unwrap()
-                .map(|v| {
-                    if v == "" {
-                        row.get_opt(4)
-                            .unwrap()
-                            .or::<String>(Ok(String::from("Anónimo")))
-                            .unwrap()
-                    } else {
-                        v
-                    }
+                .map(|v| match v.as_str() {
+                    "" => row
+                        .get_opt(4)
+                        .unwrap()
+                        .or::<String>(Ok(String::from("Anónimo")))
+                        .unwrap(),
+                    _ => v,
                 })
                 .or_else(|_| {
                     row.get_opt(4)
@@ -50,13 +57,36 @@ impl FromRow for Article {
                 .get_opt(5)
                 .unwrap()
                 .map_err(|_| FromRowError(row.clone()))?,
-        };
-        Ok(article)
+            mode: row
+                .get_opt::<String, usize>(6)
+                .unwrap()
+                .map(|v| match v.as_str() {
+                    "plaintext" => Ok(PostMode::Text),
+                    "html" => Ok(PostMode::HTML),
+                    _ => Ok(PostMode::Unknown),
+                })
+                .map_err(|_| FromRowError(row.clone()))??,
+            text: format!(
+                "{}{}",
+                row.get_opt::<String, usize>(7)
+                    .unwrap()
+                    .map_err(|_| FromRowError(row.clone()))?,
+                row.get_opt(8)
+                    .unwrap()
+                    .or::<String>(Ok(String::from("")))
+                    .unwrap(),
+            ),
+        })
     }
 }
 
-fn process(article: &Article) {
-    println!("{:?}", article);
+impl Article {
+    fn new(row: Row) -> Self {
+        from_row::<Article>(row)
+    }
+    fn dump(&self) {
+        println!("{:?}", self);
+    }
 }
 
 fn main() -> Result<()> {
@@ -65,7 +95,8 @@ fn main() -> Result<()> {
 
     let query = conn.query_iter(
         "\
-	SELECT s.sid, t.tid, s.title, u.fullname, u.username, s.date
+	SELECT s.sid, t.tid, s.title, u.fullname, u.username, s.date,
+               s.postmode, s.introtext, s.bodytext
 	FROM stories AS s INNER JOIN users AS u ON s.uid = u.uid
                           INNER JOIN topic_assignments as t ON t.id = s.sid
         ORDER BY s.date
@@ -73,7 +104,7 @@ fn main() -> Result<()> {
     )?;
 
     query.for_each(|row| match row {
-        Ok(row) => process(&from_row::<Article>(row)),
+        Ok(row) => Article::new(row).dump(),
         Err(err) => println!("Error: {}", err),
     });
 
